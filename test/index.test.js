2b11d689a7cd6b75ca963ef659037536ee953a20
Ian Liu <41207982+ianliu28@users.noreply.github.com>
2019-08-21 12:05:29 -0700

    Refactoring (#1)
    * First commit of the jira.js refactoring. Added few sample Mocha test cases.
    
    * Continuing refactoring on RFD transition part and add clock comments
    
    * Refactor "transition" function more to use workflow action name instead of "id"
    
    * added condition to throw error when argument is missing for retrieveRfcIssueInfo function
    
    * commit test case
    
    * Update dependencies/packages.json

--- test/index.test.js
+++ test/index.test.js
@@ -1,43 +1,82 @@
-'use strict';
-const expect = require('expect');
+"use strict";
+const expect = require("expect");
 
-const {Jira} = require('../index.js')
-const sinon = require('sinon');
+const { Jira } = require("../index.js");
+const sinon = require("sinon");
 const sandbox = sinon.createSandbox();
 
+// root-level hooks below (for all test files)
+let jira = null;
 
-describe('index', function() {
-  this.timeout(80000);
+beforeEach("Using fake settings to create JIRA object", function() {
+   const jiraSettings = {
+      url: "bwa123.nrs.gov.bc.ca/int/jira",
+      username: `fake`,
+      password: `fake`,
+      rfcIssueKey: `FAKE-123`,
+      changeBranch: `FAKE-123-rfc`,
+      branchName: `PR-456`,
+      repoName: `FAKE`,
+      projectName: `FAKE`,
+      version: `1.0.0`
+   };
 
-  afterEach(function () {
-    // completely restore all fakes created through the sandbox
-    sandbox.restore();
-  });
+   // Create the jira object of the type Jira
+   jira = new Jira(Object.assign({}, { phase: "jira-update", jira: jiraSettings }));
+});
+afterEach("Completely restore all fakes created through the sandbox", function() {
+   sandbox.restore();
+});
 
-  it('requires - Jira', function() {
-      expect(Jira).toEqual(expect.anything())
-  }) //end it
-  it('Intantiate - Jira', async function() {
-    const jira = new Jira({jira:{url: 'abc'}})
+describe("retrieveRfcIssueInfo(): wrapper function to retrieve RFC issue", function() {
+   context("With no argument", function() {
+      it("should throw error", async function() {
+         const issue = await jira.retrieveRfcIssueInfo(null);
+         expect(issue).toEqual(new Error("Missing 'rfcIssueKey' argument"));
+      });
+   });
+});
 
-    var stub = sandbox.stub(jira, 'createJiraClient')
-    stub.returns({
-      issue: {
-        getIssue: function(){
-          return Promise.resolve({fields: {issuelinks: []}})
-        },
-        createIssue:function (){
-          return Promise.resolve({key:'asdasda-2'})
-        }
-      },
-      issueLink:{
-        createIssueLink: function(){}
-      }
-    });
+describe("JIRA - Create RFD logic", function() {
+   this.timeout(10000);
 
-    var stub2 = sandbox.stub(jira, 'transition')
-    stub2.returns(Promise.resolve(true))
+   it("requires - Jira", function() {
+      expect(Jira).toEqual(expect.anything());
+   }); //end it
+   it("Jira - retrieveRfcIssueInfo", async function() {
+      var stub = sandbox.stub(jira, "getJiraClient");
+      stub.returns({
+         issue: {
+            getIssue: function() {
+               return Promise.resolve({ fields: { issuelinks: [], status: { name: "abc" } } });
+            },
+            createIssue: function() {
+               return Promise.resolve({ key: "asdasda-2" });
+            }
+         },
+         issueLink: {
+            createIssueLink: function() {}
+         }
+      });
 
-    jira.createRFD()
-  })
-}) //end describe
+      const issue = await jira.retrieveRfcIssueInfo("RFC-123");
+      expect(issue).toHaveProperty("fields");
+      expect(issue).toHaveProperty("fields.status.name");
+   });
+   it("Jira - initializeProjectComponent", async function() {
+      var stub = sandbox.stub(jira, "getJiraClient");
+      stub.returns({
+         project: {
+            getProject: function() {
+               return Promise.resolve({ components: [] });
+            }
+         },
+         component: {
+            createComponent: function() {
+               return Promise.resolve({});
+            }
+         }
+      });
+      const issue = await jira.initializeProjectComponent("FAKE", "fake-db");
+   });
+}); //end describe
