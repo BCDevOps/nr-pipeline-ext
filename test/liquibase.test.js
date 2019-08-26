5208a2bd12ea7ca4c0db6766eee00ac4d1bba4b6
Sivanand, Poornima FLNR:EX <Poornima.Sivanand@gov.bc.ca>
2019-08-26 12:17:26 -0700

    add test

--- test/liquibase.test.js
+++ test/liquibase.test.js
@@ -2,38 +2,45 @@
 const expect = require("expect");
 const sinon = require("sinon");
 const sandbox = sinon.createSandbox();
-const {liquibase, liquibase_version} = require('../lib/liquibase')
+const liquibase = require('../lib/liquibase')
+const settings={ phases:
+    { dev:
+       { namespace: 'fake', phase: 'fake', dburl: 'jdbc:oracle:thin:@//test.bcgov:1521/test.nrs.bcgov' },
+      },
+   options:
+    { git:
+       { dir: './',
+         branch: 'test',
+         url:'test',
+         uri: 'test',
+         http_url: 'test',
+         owner: 'test',
+         repository: 'test',
+         ref: 'refs/heads/test',
+         branch_ref:'test' },
+      cwd: './' },
+   classpath: '../others/ojdbc8.jar',
+   liquibasejar: '../liquibase-core-3.5.3.jar',
+   stages:
+    { backup:
+       { stage: 'test',
+         sql: '../BACKUP_AND_RECOVERY/sql/BACKUP.sql',
+         changelog: '../BACKUP_AND_RECOVERY/changelog/BACKUP.xml',
+         properties:
+          '../BACKUP_AND_RECOVERY/changelog_properties/backup.propeties',
+         ocSecret: 'backup-user' },
+    }}
 
 describe("liquibase module", function() {
    this.timeout(50000);
    context("On Liquibase setup", function() {
       it("install can be done", function(done) {
-         const liquibase = new Liquibase()
-         liquibase.install().then(dir=>{
+         const liquibaseObj = new liquibase(Object.assign(settings))
+         liquibaseObj.install().then(dir=>{
             expect(dir).toBeDefined();
             done()
          })
       })
-   });
-
-   context("When checking installed version", function() {
-      it("version: show correct download version", function(done) {
-         const liquibase = new Liquibase()
-         Liquibase.version().then(version=>{
-            expect(version).toBe(liquibase_version.substring(1));
-            done()
-         })
-      });
-   });
-
-   context("Running SchemaCrawler command", function() {
-      it("runCommand: SchemaCrawler -h command executed successfully. ", function(done) {
-         const schemaCrawler = new SchemaCrawler();
-         schemaCrawler.runCommand({"-h": ""}).then(output => {
-            expect(output.exitCode).toBe(0)
-            done();
-         })
-      });
    });
 
 });
