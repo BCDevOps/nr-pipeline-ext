fac569851b26a7c23685bc32815446188ae60f14
Ian Liu <41207982+ianliu28@users.noreply.github.com>
2019-08-27 13:13:32 -0700

    Separate jira.js constants into different file and add more action names. (#8)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Jira = require('./lib/jira')
 const Liquibase = require('./lib/liquibase')
+const CONST = require('./lib/constants')
 
 module.exports = {
   Jira,
-  Liquibase
+  Liquibase,
+  CONST
 }
