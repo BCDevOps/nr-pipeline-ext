13bf76397e294e3713c4aa40b48fe322b79b36db
Ian Liu <41207982+ianliu28@users.noreply.github.com>
2019-08-27 13:24:31 -0700

    export SchemaCrawler (#9)

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Jira = require('./lib/jira')
 const Liquibase = require('./lib/liquibase')
+const SchemaCrawler = require('./lib/SchemaCrawler')
 const CONST = require('./lib/constants')
 
 module.exports = {
   Jira,
   Liquibase,
+  SchemaCrawler,
   CONST
 }
