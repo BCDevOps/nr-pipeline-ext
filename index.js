ce671c1e09f1e5a7b7083aa0738444b5d2ef09e8
Sivanand, Poornima FLNR:EX <Poornima.Sivanand@gov.bc.ca>
2019-08-29 16:46:36 -0700

    index.js

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const Jira = require('./lib/jira')
 const Liquibase = require('./lib/liquibase')
 const SchemaCrawler = require('./lib/SchemaCrawler')
 const CONST = require('./lib/constants')
+const Deploy = require('./lib/BasicOracleDatabaseDeployment')
+const Build = require('./lib/build')
+const Generate = require('./createChangesetUpdateChangelog')
 
 module.exports = {
   Jira,
   Liquibase,
   SchemaCrawler,
-  CONST
+  CONST,
+  Deploy,
+  Build,
+  Generate
+
 }
