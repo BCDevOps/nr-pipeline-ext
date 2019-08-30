1fc8e4d024856abee7c40bd6498ace35aa4a7593
Ian Liu <ianliu28@gmail.com>
2019-08-30 14:11:55 -0700

    Removed   Deploy,   Build,   Generate, as these are script and will immedicatly be executed.

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ const Jira = require('./lib/jira')
 const Liquibase = require('./lib/liquibase')
 const SchemaCrawler = require('./lib/SchemaCrawler')
 const CONST = require('./lib/constants')
-const Deploy = require('./lib/BasicOracleDatabaseDeployment')
-const Build = require('./lib/build')
-const Generate = require('./createChangesetUpdateChangelog')
 
 module.exports = {
   Jira,
   Liquibase,
   SchemaCrawler,
-  CONST,
-  Deploy,
-  Build,
-  Generate
-
+  CONST
 }
