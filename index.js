b515184f37b746041eed29774a5c3f7761e16779
Sivanand, Poornima FLNR:EX <Poornima.Sivanand@gov.bc.ca>
2019-09-04 14:27:34 -0700

    add classes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const Liquibase = require('./lib/liquibase')
 const SchemaCrawler = require('./lib/SchemaCrawler')
 const CONST = require('./lib/constants')
 
+const BasicJavaApplicationBuilder = require('./lib/BasicJavaApplicationBuilder')
+
 module.exports = {
   Jira,
   Liquibase,
   SchemaCrawler,
-  CONST
+  CONST,
+  BasicJavaApplicationBuilder
 }
