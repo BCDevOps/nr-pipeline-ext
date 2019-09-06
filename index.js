3dd7471d888bc3f46da88a391e524144cb4fc3f0
Poornima Sivanand <46498006+poornima-sivanand@users.noreply.github.com>
2019-09-06 10:21:36 -0700

    Feature/add java app builder deployer (#22)
    * fix reopen issue
    
    * fix
    
    * fix transitions
    
    * test
    
    * fix
    
    * fix
    
    * add classes
    
    * Made deployment script to be a class.
    
    * add nock tests for jira
    
    * update JavaAppDeployer
    
    * update JavaAppDeployer
    
    * update JavaAppDeployer
    
    * update JavaAppDeployer

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const Liquibase = require('./lib/liquibase')
 const SchemaCrawler = require('./lib/SchemaCrawler')
 const CONST = require('./lib/constants')
 
+const BasicJavaApplicationBuilder = require('./lib/BasicJavaApplicationBuilder')
+const BasicJavaApplicationDeployer= require('./lib/BasicJavaApplicationDeployer')
+
 module.exports = {
   Jira,
   Liquibase,
   SchemaCrawler,
-  CONST
+  CONST,
+  BasicJavaApplicationBuilder,
+  BasicJavaApplicationDeployer
 }
