88baf223fc1492b0d48d0dd6ee129a9be9b606db
Sivanand, Poornima FLNR:EX <Poornima.Sivanand@gov.bc.ca>
2019-09-06 10:13:00 -0700

    update JavaAppDeployer

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const SchemaCrawler = require('./lib/SchemaCrawler')
 const CONST = require('./lib/constants')
 
 const BasicJavaApplicationBuilder = require('./lib/BasicJavaApplicationBuilder')
+const BasicJavaApplicationDeployer= require('./lib/BasicJavaApplicationDeployer')
 
 module.exports = {
   Jira,
   Liquibase,
   SchemaCrawler,
   CONST,
-  BasicJavaApplicationBuilder
+  BasicJavaApplicationBuilder,
+  BasicJavaApplicationDeployer
 }
