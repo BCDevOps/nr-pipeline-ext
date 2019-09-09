d15d6f1f3a876b40138f12b63b8aafe83d4e4a4b
Sivanand, Poornima FLNR:EX <Poornima.Sivanand@gov.bc.ca>
2019-09-09 10:40:56 -0700

    fix

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,11 @@
 "use strict";
 const expect = require("expect");
 
-const Jira = require('../lib/jira');
+const Jira = require('../lib/Jira');
 const sinon = require("sinon");
 const sandbox = sinon.createSandbox();
 
-const CONST = require('../lib/constants');
+const CONST = require('../lib/Constants');
 
 // root-level hooks below (for all test files)
 let jira = null;
