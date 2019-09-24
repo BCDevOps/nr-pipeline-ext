49446d68d6cef22f5da5bd5cee9044a37ad8de15
Ian Liu <ianliu28@gmail.com>
2019-09-24 10:03:59 -0700

    Added back backup/recovery script from old time.

--- lib/backup.sql
+++ lib/backup.sql
@@ -1,7 +1,2 @@
-SELECT sysdate FROM dual;
+call oraexp.EXPORT_UTILITY.export_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
 EXIT
-
--- call oraexp.EXPORT_UTILITY.export_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
--- truncate table databasechangelog;	
--- truncate table databasechangeloglock;
--- EXIT
--- lib/recovery.sql
+++ lib/recovery.sql
@@ -1,7 +1,2 @@
-SELECT sysdate FROM dual;
+call oraexp.IMPORT_UTILITY.import_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
 EXIT
-
--- call oraexp.IMPORT_UTILITY.import_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
--- truncate table databasechangelog;	
--- truncate table databasechangeloglock;	
--- EXIT
