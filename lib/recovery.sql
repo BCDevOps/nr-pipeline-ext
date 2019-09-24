4976793f34a16ea78b7e1e4c05a91cd6c623b040
Ian Liu <ianliu28@gmail.com>
2019-09-24 09:35:43 -0700

    Added recoery.sql and rename node module to dbBackupRecovery.js and adjusted code to handle recovery.

---
+++ lib/recovery.sql
@@ -0,0 +1,7 @@
+SELECT sysdate FROM dual;
+EXIT
+
+-- call oraexp.IMPORT_UTILITY.import_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
+-- truncate table databasechangelog;	
+-- truncate table databasechangeloglock;	
+-- EXIT
