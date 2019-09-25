d479528888f8dda78504e5e81c096e0c3df57bd4
Ian Liu <ianliu28@gmail.com>
2019-09-25 09:42:55 -0700

    Added 'WHENEVER SQLERROR EXIT SQL.SQLCODE' so that container can return exit code to node spawn

--- lib/backup.sql
+++ lib/backup.sql
@@ -1,2 +1,6 @@
-call oraexp.EXPORT_UTILITY.export_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+call auto_bkp_reco.EXPORT_UTILITY.export_schema(oraexp.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
 EXIT
+
+-- select sysdate from dual;
+-- EXIT
