7b0c1429d958fd25ddf1e84b7d25dcf708baa616
Ian Liu <ianliu28@gmail.com>
2019-09-26 12:11:07 -0700

    remove static and not generic backup.sql and recover.sql

--- lib/backup.sql
+++
@@ -1,6 +0,0 @@
-WHENEVER SQLERROR EXIT SQL.SQLCODE
-EXEC auto_bkp_reco.EXPORT_UTILITY.export_schema(auto_bkp_reco.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
-EXIT
-
--- select sysdate from dual;
--- EXIT
--- lib/recovery.sql
+++
@@ -1,3 +0,0 @@
-WHENEVER SQLERROR EXIT SQL.SQLCODE
-EXEC auto_bkp_reco.IMPORT_UTILITY.import_schema(auto_bkp_reco.t_param_list('CWI_SPI_DC','CWI_TXN'),'DATAPUMP_DIR','logfile.log','dumpfile.dmp');
-EXIT
