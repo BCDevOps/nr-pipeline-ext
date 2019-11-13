36aa437a68b2f4e7b61b19d532e83b6ec0905c86
Poornima Sivanand <46498006+poornima-sivanand@users.noreply.github.com>
2019-11-12 16:19:32 -0800

    Feature/update liquibase (#51)
    * test
    
    * update liquibase
    
    * Test
    
    * tried resolving async problem
    
    * updated formating
    
    * Fixed of the Liquibase from Clecio
    
    * add update-liquibase
    
    * using upper case Liquibase when required
    
    * removed -v for untar not going to 'on.("exit)' event issue
    
    * Update Liquibase.js
    
    * test
    
    * Update basicOracleDatabaseDeployment.js

--- test/liquibase.test.js
+++ test/liquibase.test.js
@@ -1,13 +1,13 @@
 "use strict";
 const expect = require("expect");
-const liquibase = require('../lib/liquibase')
+const Liquibase = require('../lib/Liquibase')
 
 describe("liquibase module", function() {
-   this.timeout(1000000);
+   this.timeout(50000);
    context("On Liquibase setup", function() {
       it.only("install can be done", function() {
          const idir = require("./idir.local.json");
-         const liquibaseObj = new liquibase({drivers:[{groupId:'com.oracle.jdbc', artifactId: 'ojdbc8', version: '18.3.0.0'}], credentials: idir})
+         const liquibaseObj = new Liquibase({drivers:[{groupId:'com.oracle.jdbc', artifactId: 'ojdbc8', version: '18.3.0.0'}], credentials: idir})
 
          return liquibaseObj.run(['--version']).then(proc=>{
             expect(proc.stdout.split('\n')[1]).toBe('Liquibase Version: 3.8.0');
