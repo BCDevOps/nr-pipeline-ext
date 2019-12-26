71e9cfd46cc2e917c5ed9fe03854c3b86347ab2b
Ian Liu <ianliu28@gmail.com>
2019-12-26 08:08:20 -0800

    Added new test

--- test/InputDeployerVerify.test.js
+++ test/InputDeployerVerify.test.js
@@ -2,7 +2,7 @@
 const Jira = require('../lib/Jira');
 const expect = require("expect");
 var sandbox = require('sinon').createSandbox();
-const {ENV, ISSUE_STATUS_NAME, ISSUE_LINK_TYPE_NAME} = require("../lib/constants");
+const {ENV, ISSUE_STATUS_NAME, ISSUE_LINK_TYPE_NAME, VERIFY_STATUS, REASON} = require("../lib/constants");
 const {previousEnv} = require("../lib/util-functions");
 const Verifier = require("../lib/InputDeployerVerify");
 
@@ -18,82 +18,112 @@ describe("obtainCurrentRfcRfdContext:", function() {
         sandbox.restore();
     });
 
-    it("At 'dlvr' env, With rfc/rfd issues can be found, returns Rfc/Rfd context for caller function", async function() {
-        // Arrange
-        const rfcIssueKeyStub = 'MyRFCissue-99';
-        const rfcIssueStub = getDefaultRfcIssue();
+    context("Function isReadyForDeployment()", function() {
+        it.only("When RFDs blockedBy others, return result: status='Not Ready' , rfcRfdContext and reasonn with code: REASON_CODE_RFD_BLOCKED", async function() {
+            // Arrange
+            const rfcIssueKeyStub = 'MyRFCissue-99';
+            const env = 'test';
+            const settings = getDefaultSettings();
+            settings.options.env = env;
+            settings.phase = env;
+            const verifier = new Verifier(settings);
+            const blockedByRfcRfdContext = {"rfcIssueKey":"MyRFCissue-99","rfcStatus":"Authorized for Test","rfdsByEnv":{"env":"test","rfds":[{"rfdIssueKey":"RFD-AUTO-TEST-01","labels":"auto","env":"test","status":"Approved","blockedBy":[{"issueKey":"INWARDISSUE-0","status":"Some Other Status","blockingOn":"RFD-AUTO-TEST-01"}]},{"rfdIssueKey":"RFD-BUSINESS-TEST-01","labels":"some-label","env":"test","status":"Some Other Status","blockedBy":[]}],"previousEnvRfds":[{"rfdIssueKey":"RFD-AUTO-DLVR-01","env":"dlvr","status":"Closed","labels":"auto"}]}};
+            const rfcIssueKey = blockedByRfcRfdContext.rfcIssueKey;
+            sandbox.stub(verifier, 'obtainCurrentRfcRfdContext').returns(Promise.resolve(blockedByRfcRfdContext));
 
-        jiraClientStub.retrieveRfcIssueInfo.resolves(rfcIssueStub);
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_DLVR_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_DLVR_STUB));
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_STUB));
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_BUSINESS_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_BUSINESS_STUB));
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_PROD_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_PROD_STUB));
+            // Act
+            const result = await verifier.isReadyForDeployment(env, rfcIssueKey);
+            console.log('result:', result);
 
-        // Act
-        const dlvrEnv = 'dlvr';
-        const dlvrVerifier = new Verifier(getDefaultSettings());
-        const dlvrRfcRfdContext = await dlvrVerifier.obtainCurrentRfcRfdContext(dlvrEnv, rfcIssueKeyStub);
-
-        // Verify
-        sandbox.assert.calledOnce(jiraClientStub.retrieveRfcIssueInfo);
-        sandbox.assert.calledWith(jiraClientStub.retrieveRfcIssueInfo, rfcIssueKeyStub);
-        const getCalledCount = rfcIssueStub.fields.issuelinks.filter(link => link.type.name === ISSUE_LINK_TYPE_NAME.RFC_FRD).reduce((accumulator, link) => { return accumulator + 1}, 0);
-        sandbox.assert.callCount(jiraClientStub.getIssue, getCalledCount);
-        expect(dlvrRfcRfdContext.rfcIssueKey).toBe(rfcIssueKeyStub);
-        expect(dlvrRfcRfdContext.rfdsByEnv.env).toEqual(dlvrEnv);
-        expect(dlvrRfcRfdContext.rfdsByEnv.rfds).toBeInstanceOf(Array);
-        dlvrRfcRfdContext.rfdsByEnv.rfds.forEach((rfdInfo) => {
-            expect(rfdInfo.rfdIssueKey).toEqual(RFD_ISSUE_KEY_DLVR_STUB);
-            expect(rfdInfo.blockedBy).toBeInstanceOf(Array);
-            expect(rfdInfo.env).toEqual(dlvrEnv);
-            if (rfdInfo.blockedBy) {
-                rfdInfo.blockedBy.forEach((blockedByIssue) => {
-                    expect(blockedByIssue.blockingOn).toEqual(rfdInfo.rfdIssueKey);
-                })
-            }
+            // Verify
+            sandbox.assert.calledWith(verifier.obtainCurrentRfcRfdContext, env, rfcIssueKey);
+            expect(result.status).toEqual(VERIFY_STATUS.NOT_READY);
+            expect(Object.keys(result)).toEqual(expect.arrayContaining(["status", "rfcRfdContext", "reason"]));
+            expect(result.rfcRfdContext).toEqual(blockedByRfcRfdContext);
+            expect(result.reason.code).toEqual(REASON.REASON_CODE_RFD_BLOCKED);
+            expect(result.reason.issueItems.length).not.toBe(0);
         });
-        expect(dlvrRfcRfdContext.rfdsByEnv.previousEnvRfds).toBeInstanceOf(Array);
-        expect(dlvrRfcRfdContext.rfdsByEnv.previousEnvRfds.every(p => p.env == previousEnv(dlvrEnv))).toBe(true);
-        
-    });
+    })
 
-    it("At 'test' env, With rfc/rfd issues can be found, returns Rfc/Rfd context for caller function", async function() {
-        // Arrange
-        const rfcIssueKeyStub = 'MyRFCissue-99';
-        const rfcIssueStub = getDefaultRfcIssue();
-
-        jiraClientStub.retrieveRfcIssueInfo.resolves(rfcIssueStub);
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_DLVR_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_DLVR_STUB));
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_STUB));
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_BUSINESS_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_BUSINESS_STUB));
-        jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_PROD_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_PROD_STUB));
-
-        // Act
-        const testEnv = 'test';
-        const testVerifier = new Verifier(getDefaultSettings());
-        const testRfcRfdContext = await testVerifier.obtainCurrentRfcRfdContext(testEnv, rfcIssueKeyStub);
-        console.log('testRfcRfdContext', JSON.stringify(testRfcRfdContext));
-        // Verify
-        sandbox.assert.calledOnce(jiraClientStub.retrieveRfcIssueInfo);
-        sandbox.assert.calledWith(jiraClientStub.retrieveRfcIssueInfo, rfcIssueKeyStub);
-        const getCalledCount = rfcIssueStub.fields.issuelinks.filter(link => link.type.name === ISSUE_LINK_TYPE_NAME.RFC_FRD).reduce((accumulator, link) => { return accumulator + 1}, 0);
-        sandbox.assert.callCount(jiraClientStub.getIssue, getCalledCount);
-        expect(testRfcRfdContext.rfcIssueKey).toBe(rfcIssueKeyStub);
-        expect(testRfcRfdContext.rfdsByEnv.env).toEqual(testEnv);
-        expect(testRfcRfdContext.rfdsByEnv.rfds).toBeInstanceOf(Array);
-        expect(testRfcRfdContext.rfdsByEnv.rfds.map(rfd => rfd.rfdIssueKey)).toEqual(expect.arrayContaining(['RFD-AUTO-TEST-01','RFD-BUSINESS-TEST-01']));
-        testRfcRfdContext.rfdsByEnv.rfds.forEach((rfdInfo) => {
-            expect(rfdInfo.blockedBy).toBeInstanceOf(Array);
-            expect(rfdInfo.env).toEqual(testEnv);
-            if (rfdInfo.blockedBy) {
-                rfdInfo.blockedBy.forEach((blockedByIssue) => {
-                    expect(blockedByIssue.blockingOn).toEqual(rfdInfo.rfdIssueKey);
-                })
-            }
+    context("Function obtainCurrentRfcRfdContext()", function() {
+        it("At 'dlvr' env, With rfc/rfd issues can be found, returns Rfc/Rfd context for caller function", async function() {
+            // Arrange
+            const rfcIssueKeyStub = 'MyRFCissue-99';
+            const rfcIssueStub = getDefaultRfcIssue();
+    
+            jiraClientStub.retrieveRfcIssueInfo.resolves(rfcIssueStub);
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_DLVR_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_DLVR_STUB));
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_STUB));
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_BUSINESS_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_BUSINESS_STUB));
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_PROD_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_PROD_STUB));
+    
+            // Act
+            const dlvrEnv = 'dlvr';
+            const dlvrVerifier = new Verifier(getDefaultSettings());
+            const dlvrRfcRfdContext = await dlvrVerifier.obtainCurrentRfcRfdContext(dlvrEnv, rfcIssueKeyStub);
+    
+            // Verify
+            sandbox.assert.calledOnce(jiraClientStub.retrieveRfcIssueInfo);
+            sandbox.assert.calledWith(jiraClientStub.retrieveRfcIssueInfo, rfcIssueKeyStub);
+            const getCalledCount = rfcIssueStub.fields.issuelinks.filter(link => link.type.name === ISSUE_LINK_TYPE_NAME.RFC_FRD).reduce((accumulator, link) => { return accumulator + 1}, 0);
+            sandbox.assert.callCount(jiraClientStub.getIssue, getCalledCount);
+            expect(dlvrRfcRfdContext.rfcIssueKey).toBe(rfcIssueKeyStub);
+            expect(dlvrRfcRfdContext.rfdsByEnv.env).toEqual(dlvrEnv);
+            expect(dlvrRfcRfdContext.rfdsByEnv.rfds).toBeInstanceOf(Array);
+            dlvrRfcRfdContext.rfdsByEnv.rfds.forEach((rfdInfo) => {
+                expect(rfdInfo.rfdIssueKey).toEqual(RFD_ISSUE_KEY_DLVR_STUB);
+                expect(rfdInfo.blockedBy).toBeInstanceOf(Array);
+                expect(rfdInfo.env).toEqual(dlvrEnv);
+                if (rfdInfo.blockedBy) {
+                    rfdInfo.blockedBy.forEach((blockedByIssue) => {
+                        expect(blockedByIssue.blockingOn).toEqual(rfdInfo.rfdIssueKey);
+                    })
+                }
+            });
+            expect(dlvrRfcRfdContext.rfdsByEnv.previousEnvRfds).toBeInstanceOf(Array);
+            expect(dlvrRfcRfdContext.rfdsByEnv.previousEnvRfds.every(p => p.env == previousEnv(dlvrEnv))).toBe(true);
+            
         });
-        expect(testRfcRfdContext.rfdsByEnv.previousEnvRfds).toBeInstanceOf(Array);
-        expect(testRfcRfdContext.rfdsByEnv.previousEnvRfds.every(p => p.env == previousEnv(testEnv))).toBe(true);
-    });
+    
+        it("At 'test' env, With rfc/rfd issues can be found, returns Rfc/Rfd context for caller function", async function() {
+            // Arrange
+            const rfcIssueKeyStub = 'MyRFCissue-99';
+            const rfcIssueStub = getDefaultRfcIssue();
+    
+            jiraClientStub.retrieveRfcIssueInfo.resolves(rfcIssueStub);
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_DLVR_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_DLVR_STUB));
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_STUB));
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_TEST_BUSINESS_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_TEST_BUSINESS_STUB));
+            jiraClientStub.getIssue.withArgs(RFD_ISSUE_KEY_PROD_STUB).resolves(getDefaultRfdIssueInfo(RFD_ISSUE_KEY_PROD_STUB));
+    
+            // Act
+            const testEnv = 'test';
+            const testVerifier = new Verifier(getDefaultSettings());
+            const testRfcRfdContext = await testVerifier.obtainCurrentRfcRfdContext(testEnv, rfcIssueKeyStub);
+            // console.log('testRfcRfdContext', JSON.stringify(testRfcRfdContext));
+
+            // Verify
+            sandbox.assert.calledOnce(jiraClientStub.retrieveRfcIssueInfo);
+            sandbox.assert.calledWith(jiraClientStub.retrieveRfcIssueInfo, rfcIssueKeyStub);
+            const getCalledCount = rfcIssueStub.fields.issuelinks.filter(link => link.type.name === ISSUE_LINK_TYPE_NAME.RFC_FRD).reduce((accumulator, link) => { return accumulator + 1}, 0);
+            sandbox.assert.callCount(jiraClientStub.getIssue, getCalledCount);
+            expect(testRfcRfdContext.rfcIssueKey).toBe(rfcIssueKeyStub);
+            expect(testRfcRfdContext.rfdsByEnv.env).toEqual(testEnv);
+            expect(testRfcRfdContext.rfdsByEnv.rfds).toBeInstanceOf(Array);
+            expect(testRfcRfdContext.rfdsByEnv.rfds.map(rfd => rfd.rfdIssueKey)).toEqual(expect.arrayContaining(['RFD-AUTO-TEST-01','RFD-BUSINESS-TEST-01']));
+            testRfcRfdContext.rfdsByEnv.rfds.forEach((rfdInfo) => {
+                expect(rfdInfo.blockedBy).toBeInstanceOf(Array);
+                expect(rfdInfo.env).toEqual(testEnv);
+                if (rfdInfo.blockedBy) {
+                    rfdInfo.blockedBy.forEach((blockedByIssue) => {
+                        expect(blockedByIssue.blockingOn).toEqual(rfdInfo.rfdIssueKey);
+                    })
+                }
+            });
+            expect(testRfcRfdContext.rfdsByEnv.previousEnvRfds).toBeInstanceOf(Array);
+            expect(testRfcRfdContext.rfdsByEnv.previousEnvRfds.every(p => p.env == previousEnv(testEnv))).toBe(true);
+        });
+    })
 
 });
 
