7c459a1e8324cce99170621dce1a527393aa8268
Sivanand, Poornima FLNR:EX <Poornima.Sivanand@gov.bc.ca>
2019-12-31 08:30:03 -0800

    test

--- test/CreateChangesetUpdateChangelog.test.js
+++ test/CreateChangesetUpdateChangelog.test.js
@@ -3,11 +3,13 @@ const expect = require('expect')
 const moment = require('moment-timezone')
 const Generator = require("../lib/CreateChangesetUpdateChangelog")
 const generate=new Generator()
+const fs = require('fs')
+const sinon= require('sinon')
 
  describe("create Changeset", function() {
    this.timeout(50000);
    context("On running succesfully", function() {
-      it.only("creates changeset", function() {
+      it("creates changeset", function() {
         var tDate=moment.tz(Date.now(),'America/Vancouver').format('YYYYMMDDHHmmss')
         var str="../migrations/testschema/sql/V"+tDate+"01__SAMPLE-123_create-table.sql"
          expect(generate.createChangeset("create-table","../migrations/testschema/sql","SAMPLE-123","01")).toBe(str)
@@ -18,7 +20,7 @@ const generate=new Generator()
     describe("isDir", function() {
     this.timeout(50000);
     context("On running ", function() {
-       it.only("returns true if directory exists, else false", function() {
+       it("returns true if directory exists, else false", function() {
           expect(generate.isDir('../migrations/testschema')).toBe(true)
           })
        })
@@ -26,15 +28,16 @@ const generate=new Generator()
     describe("isDir", function() {
         this.timeout(50000);
         context("On running ", function() {
-           it.only("returns dalse if directory doesnt exist", function() {
+           it("returns dalse if directory doesnt exist", function() {
               expect(generate.isDir('../migrations/test')).toBe(false)
               })
            })
         }); 
+        
  describe("update changelog", function() {
         this.timeout(50000);
         context("On running ", function() {
-           it.only("updates a file", function() {
+           it("updates a file", function() {
               expect(generate.updateChangelog('../migrations/testschema/changelog/testschema.xml',['create-table'],'test','99881891991')).toBe(true)
               })
            })
@@ -44,7 +47,9 @@ describe("generate", function() {
         this.timeout(50000);
         context("On running ", function() {
             it.only("creates changeset and updates changelog", function() {
-                expect(generate.execute('testschema','test','SAMPLE-123','create-table')).toBe(true)
+                expect(generate.execute('testschema','test','SAMPLE-123','create-table','../migrations'))
                 })
             })
         });  
+
+       
