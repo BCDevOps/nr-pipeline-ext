d56cda4c379d3e5dac5013182bc47333ee2e0607
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2020-02-24 16:53:19 -0800

    Update GitOperation.test.js

--- test/GitOperation.test.js
+++ test/GitOperation.test.js
@@ -4,11 +4,11 @@ const sinon = require("sinon");
 const Gitop = require('../lib/GitOperation')
 
 describe("GitOperation: @slow", function() {
-   const idir = require("./idir.local.json");
    this.timeout(50000);
    // system test, requires credential from "./idir.local.json" (create one if not exists)
    context("prepare()", function() {
       it("When successfully prepared, repo directory is returned.", function() {
+         const idir = require("./idir.local.json");
          // const gitCredentials = {username: 'fakeUsername', password: 'fakePass', email: 'fakeEmail'}
          const gitCredentials = {username: idir.user, password: idir.pass, email: 'fakeEmail'};
          const git = new Gitop('/Users/iliu/Workspace/labs/tempDir', 'https://bwa.nrs.gov.bc.ca/int/stash/scm/spi/spi-schema-da-review.git', gitCredentials, 'test');
