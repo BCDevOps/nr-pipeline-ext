d912c7a9020ed7779206c81c08354cacfa813d46
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2020-02-24 16:57:56 -0800

    Update gitVerification.test.js

--- test/gitVerification.test.js
+++ test/gitVerification.test.js
@@ -1,7 +1,6 @@
 "use strict";
 const expect = require("expect");
 const Git = require('../lib/GitVerification')
-const idir=require('./idir.local.json')
 
 describe("git module", function() {
    this.timeout(50000);
@@ -36,8 +35,8 @@ describe("git module", function() {
     this.timeout(50000);
     context("On running git ls-remote", function() {
        it.only("latest commit hash on master can be obtained", function() {
+          const idir=require('./idir.local.json')
           const gitObj = new Git()
- 
           return gitObj.getLatestCommitOnTarget(idir.user,idir.pass,idir.url, 'master').then(commitHash=>{
              expect(commitHash).toBeDefined()
           })
@@ -50,8 +49,8 @@ describe("git module", function() {
     this.timeout(50000);
     context("On running verify", function() {
        it.only("verify if branches can be merged", function() {
+          const idir=require('./idir.local.json')
           const gitObj = new Git()
- 
           return gitObj.verify('release/0.0.1','master',idir.user,idir.pass,idir.url).then(result=>{
              expect(result).toBe('True')
           })
@@ -64,6 +63,7 @@ describe("git module", function() {
     this.timeout(50000);
     context("On running verify", function() {
        it.only("verify if branches can be merged", async function() {
+          const idir=require('./idir.local.json')
           const gitObj = new Git()
             return expect(gitObj.verify('master','release/0.0.1',idir.user,idir.pass,idir.url)).rejects.toThrow()
        })
