df6247fdc3f491333f97ba087d1089808d091e4b
Ian Liu <ian.liu@gov.bc.ca>
2020-03-20 17:21:13 -0700

    Add color settings for the logger

--- config/log-settings.js
+++ config/log-settings.js
@@ -12,11 +12,29 @@ const logSettings = {
         defaultLogConfig: {
             transports: [
                 new transports.File({
-                    level: 'debug',
+                    level: 'error',
                     filename: path.join(__dirname, '..', 'logs', 'trace.log'),
                 }),
                 new transports.Console({
-                    format: combine(label({ label: 'nr-pipeline-ext' }), timestamp(), myFormat),
+                    level: 'debug',
+                    colorize: true,
+                    format: combine(
+                        label({ label: 'nr-pipeline-ext' }),
+                        timestamp(),
+                        format.align(),
+                        format.prettyPrint(),
+                        format.colorize({
+                            all: true, // this is important otherwise only 'level' will be colored, not the message.
+                            colors: {
+                                error: 'bold red',
+                                warn: 'bold yellow',
+                                info: 'green',
+                                verbose: 'cyan',
+                                debug: 'white',
+                            },
+                        }),
+                        myFormat
+                    ),
                 }),
             ],
             exitOnError: false,
