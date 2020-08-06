6883cd80f336158933e272cca59d2c73f67e5355
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2020-08-06 12:08:16 -0700

    Update RFC Workflow
    Initial TypeScript support

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const BasicJavaApplicationDeployer = require('./lib/BasicJavaApplicationDeployer
 const CreateChangesetUpdateChangelog = require('./lib/CreateChangesetUpdateChangelog')
 const BasicJavaApplicationClean = require('./lib/BasicJavaApplicationClean')
 const Initializer = require('./lib/InitializeJavaAppClass')
+const RfcWorkflow = require('./lib/JiraRfcWorkflowV2.0.0')
+const RfdWorkflow = require('./lib/JiraRfdWorkflowV1.2')
 
 module.exports = {
     Jira,
@@ -21,4 +23,6 @@ module.exports = {
     BasicJavaApplicationClean,
     InputDeployerVerify,
     Initializer,
+    RfcWorkflow,
+    RfdWorkflow,
 }
